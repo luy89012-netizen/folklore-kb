f8f99242627a0ddd74081fa8b975660552e1566a
luy89012-netizen <luy89012@gmail.com>
2026-07-16 17:50:00 +0800

    style: update src/main.tsx

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -5,6 +5,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './theme.css'
 import './main.css'
 
 const container = document.getElementById('miniapp-root')
